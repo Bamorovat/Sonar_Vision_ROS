c723309ba3009a0e959c26a6bfe87b5cdb613fc8
Mohammad Hossein Bamorovat Abadi <m.bamorovvat@ymail.com>
2019-02-14 10:06:21 +0000

    Create Navigation.h

---
+++ include/visual_sonar_ros/Navigation.h
@@ -0,0 +1,22 @@
+/*	Visual_Sonar: An algorithm which can find a free path via the image.
+
+Copyright (C) 2016 Mohammad Hossein Bamorovat Abadi
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include <math.h>
+
+void navigation (int X_Vector_Sum, int Y_Vector_Sum, float &Angular_Velocity , float &Linear_Velocity );
